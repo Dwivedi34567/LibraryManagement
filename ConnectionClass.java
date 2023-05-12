84e4b81dbe6044b0e1c240593889b52b588e9db1
Dwivedi34567 <115071415+Dwivedi34567@users.noreply.github.com>
2023-05-13 01:06:25 +0530

    Add files via upload

---
+++ ConnectionClass.java
@@ -0,0 +1,22 @@
+package Library;
+import java.sql.*;
+public class ConnectionClass {
+    Connection con;
+    Statement stm;
+    ConnectionClass(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/Library","root","abhay@123");
+            stm=con.createStatement();
+            
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String args[]){
+        new ConnectionClass();
+    }
+}
+
+   
